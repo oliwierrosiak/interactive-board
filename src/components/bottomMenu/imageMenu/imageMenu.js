efb84a29d33c2484fdbf08b5280089a77d50162d
Oliwier <oliwierrosiak07@gmail.com>
2025-11-19 12:04:13 +0100

    Added border width menu

--- src/components/bottomMenu/imageMenu/imageMenu.js
+++ src/components/bottomMenu/imageMenu/imageMenu.js
@@ -10,7 +10,7 @@ function ImageMenu(props)
     const borderColorSetter = ()=>{
         if(props.element.class)
         {
-            const borderColor = props.element.class.find(x=>x.includes('border'))
+            const borderColor = props.element.class.find(x=>x.includes('borderBg'))
             return borderColor || 'borderBgBlack6'
         }
         else
@@ -19,11 +19,23 @@ function ImageMenu(props)
         }
     }
 
+    const borderWidthSetter = () =>{
+        if(props.element.class)
+        {
+            const borderWidth = props.element.class.find(x=>x.includes('borderWidth'))
+            return borderWidth || 'borderWidth1'
+        }
+        else
+        {
+            return 'borderWidth1'
+        }
+    }
+
     const [display,setDisplay] = useState(false)
     const [displayColorMenu,setDisplayColorMenu] = useState(false)
     const [borderColor,setBorderColor] = useState(borderColorSetter())
     const [displayBorderWidthMenu,setDisplayBorderWidthMenu] = useState(false)
-    const [borderWidth,setBorderWidth] = useState()
+    const [borderWidth,setBorderWidth] = useState(borderWidthSetter())
 
    useEffect(()=>{
         setTimeout(()=>{
@@ -32,6 +44,12 @@ function ImageMenu(props)
     },[props.display])
 
     const setBorderColorFunc = (color) =>{
+        if(borderWidth === "borderWidth1")
+        {
+            props.element.removeClass("borderWidth1")
+            props.element.addClass(`borderWidth3`)
+        }
+        setBorderWidth(`borderWidth3`)
         setBorderColor(color)
         props.setEditUpdate(!props.editUpdate)
     }
@@ -47,19 +65,69 @@ function ImageMenu(props)
         return array.join('')
     }
 
+    const borderWidthClicked = (width) =>
+    {
+        const classes = props.element.class
+        const filtered = classes.filter(x => x.includes('borderWidth'))
+        filtered.forEach(x => {
+            props.element.removeClass(x)
+        });
+        const newClass = `borderWidth${width}`
+        props.element.addClass(newClass)
+        setBorderWidth(newClass)
+        props.setEditUpdate(!props.editUpdate)
+    }
+
+    const boardEvent = (e) =>{
+        if(!e.target.classList.contains(styles.borderColor) && !e.target.classList.contains(styles.colorPreview))
+        {
+            setDisplayColorMenu(false)
+        }
+        if(!e.target.classList.contains(styles.borderWidth) && !e.target.classList.contains(styles.borderWidthPreview))
+        {
+            setDisplayBorderWidthMenu(false)
+        }
+    }
+
     useEffect(()=>{
         setBorderColor(borderColorSetter())
+        setBorderWidth(borderWidthSetter())
     },[props.element])
+
+    useEffect(()=>{
+        window.addEventListener('click',boardEvent)
+        return () =>{
+            window.removeEventListener('click',boardEvent)
+        }
+    },[])
 
     return (
         <div className={`${styles.container} ${display?styles.display:''}`}>
-            <div className={styles.item} onClick={e=>setDisplayColorMenu(!displayColorMenu)}>
+            <div className={`${styles.item} ${styles.borderColor}`} onClick={e=>setDisplayColorMenu(!displayColorMenu)}>
                 <div className={`${styles.colorPreview} ${borderColors[borderColor]}`}></div>
                 {displayColorMenu && <BgColorMenu changeBgColor={setBorderColorFunc} border={true} color={colorSetter()} item={props.element}/>}
             </div>
-            <div className={styles.item} onClick={e=>setDisplayBorderWidthMenu(!displayBorderWidthMenu)}>
+            <div className={`${styles.item} ${styles.borderWidth}`} onClick={e=>setDisplayBorderWidthMenu(!displayBorderWidthMenu)}>
+                <div className={styles.borderWidthPreview}></div>
                 {displayBorderWidthMenu && <ul className={styles.widthMenu}>
-                    <li>test</li>
+                    <li className={`${styles.borderWidthOption} ${borderWidth==="borderWidth6"?styles.selected:''}`} onClick={e=>borderWidthClicked(6)}>
+                        <div className={styles.width6}></div>
+                    </li>
+                    <li className={`${styles.borderWidthOption} ${borderWidth==="borderWidth5"?styles.selected:''}`} onClick={e=>borderWidthClicked(5)}>
+                        <div className={styles.width5}></div>
+                    </li>
+                    <li className={`${styles.borderWidthOption} ${borderWidth==="borderWidth4"?styles.selected:''}`} onClick={e=>borderWidthClicked(4)}>
+                        <div className={styles.width4}></div>
+                    </li>
+                    <li className={`${styles.borderWidthOption} ${borderWidth==="borderWidth3"?styles.selected:''}`} onClick={e=>borderWidthClicked(3)}>
+                        <div className={styles.width3}></div>
+                    </li>
+                    <li className={`${styles.borderWidthOption} ${borderWidth==="borderWidth2"?styles.selected:''}`} onClick={e=>borderWidthClicked(2)}>
+                        <div className={styles.width2}></div>
+                    </li>
+                    <li className={`${styles.borderWidthOption} ${borderWidth==="borderWidth1"?styles.selected:''}`} onClick={e=>borderWidthClicked(1)}>
+                        <p>Brak</p>
+                    </li>
                 </ul>}
             </div>
             <div className={styles.item}>ścięcia narożników</div>
