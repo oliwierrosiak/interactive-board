af19c8e4aba8d150ac6490c159653b451b92a09e
Oliwier <oliwierrosiak07@gmail.com>
2025-12-25 23:36:58 +0100

    Added colors and fonts

--- src/assets/svg/errorIcon.js
+++ src/assets/svg/errorIcon.js
@@ -2,9 +2,9 @@ function ErrorIcon(props)
 {
     return(
         <svg className={props.class?props.class:''} width="100%" height="100%" viewBox="0 0 24 24" fill="none">
-<path d="M12 8V12" stroke="#ff0000ff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M12 16.0195V16" stroke="#ff0000ff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<circle cx="12" cy="12" r="10" stroke="#ff0000ff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M12 8V12" stroke="#EAB308" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M12 16.0195V16" stroke="#EAB308" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<circle cx="12" cy="12" r="10" stroke="#EAB308" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
     )
 }
