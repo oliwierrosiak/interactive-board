0c1e89feedd4370a67a577d8a1ada68c557853e7
Oliwier <oliwierrosiak07@gmail.com>
2026-01-02 17:19:08 +0100

    Login protected while loading

--- src/components/login/login/login.js
+++ src/components/login/login/login.js
@@ -5,48 +5,90 @@ import PasswordEyeHidden from '../../../assets/svg/passwordEyeHidden'
 import GoogleIcon from '../../../assets/svg/googleIcon'
 import DisplayLoginContext from '../../../context/displayLogin'
 import { divClicked,inputBlur,inputFocused } from '../inputActions'
+import LoadingIcon from '../../../assets/svg/loadingIcon'
+import axios from 'axios'
+import ApiAddress from '../../../ApiAddress'
 
 function Login(props)
 {
     const [loginValue,setLoginValue] = useState('')
     const [passwordValue,setPasswordValue] = useState('')
     const [showPassword,setShowPassword] = useState(false)
+    const [error,setError] = useState('')
 
     const displayLoginContext = useContext(DisplayLoginContext)
 
+    const sendData = async(e) =>
+    {
+        try
+        {
+            props.setLoading(true)
+            setShowPassword(false)
+            const response = await axios.post(`${ApiAddress}/login`,{email:loginValue,password:passwordValue})
+        }
+        catch(ex)
+        {
+            console.log(ex)
+            props.setLoading(false)
+        }
+    }
+
+    const submit = (e) =>{
+        e.preventDefault()
+        if(props.loading) return
+        setError('')
+        if(loginValue && passwordValue)
+        {
+            sendData()
+        }
+        else
+        {
+            setError('Podaj email i hasło')
+        }
+    }
+
+    const googleLogin = (e) =>
+    {
+        console.log('googleLogin')
+    }
+
     return(
         <div className={`${styles.loginForm} ${props.display?styles.display:''}`}>
-            <h1 className={styles.header}>Logowanie</h1>
-            <form className={styles.form}>
 
-                <div className={`${styles.inputContainer} ${styles.emailInputContainer}`} onClick={divClicked}>
-                    <input value={loginValue} onChange={e=>setLoginValue(e.target.value)} type='text' onBlur={inputBlur} onFocus={inputFocused} className={styles.input}></input>
+            <h1 className={styles.header}>Logowanie</h1>
+
+            <form className={styles.form} noValidate onSubmit={submit}>
+
+                <div className={`${styles.inputContainer} ${styles.emailInputContainer} ${props.loading?styles.inputContainerWhileLoading:''}`} onClick={divClicked}>
+                    <input disabled={props.loading} value={loginValue} onChange={e=>setLoginValue(e.target.value)} type='email' onBlur={inputBlur} onFocus={inputFocused} className={styles.input}></input>
                     <div className={styles.placeholder}>Podaj email</div>
                 </div>
 
-                <div className={styles.inputContainer} onClick={divClicked}>
-                    <input value={passwordValue} onChange={e=>setPasswordValue(e.target.value)} type={showPassword?'text':'password'} onBlur={inputBlur} onFocus={inputFocused} className={`${styles.input} ${styles.passwordInput}`}></input>
+                <div className={`${styles.inputContainer} ${props.loading?styles.inputContainerWhileLoading:''}`} onClick={divClicked}>
+                    <input disabled={props.loading} value={passwordValue} onChange={e=>setPasswordValue(e.target.value)} type={showPassword?'text':'password'} onBlur={inputBlur} onFocus={inputFocused} className={`${styles.input} ${styles.passwordInput}`}></input>
                     <div className={styles.placeholder}>Podaj hasło</div>
-                    <div className={styles.eye} onClick={e=>setShowPassword(!showPassword)}>
+                    <div className={styles.eye} onClick={e=>!props.loading && setShowPassword(!showPassword)}>
                         {showPassword?<PasswordEye />:<PasswordEyeHidden />}
                     </div>
                 </div>
 
-                <div className={styles.passwordForgotten} onClick={e=>displayLoginContext.setDisplayLogin('passwordForgotten')}>Nie pamiętasz hasła?</div>
+                <div className={`${styles.passwordForgotten} ${props.loading?styles.passwordForgottenWhileLoading:''}`} onClick={e=>!props.loading && displayLoginContext.setDisplayLogin('passwordForgotten')}>Nie pamiętasz hasła?</div>
 
-                <button className={styles.loginBtn}>Zaloguj się</button>
+                {error && <div className={styles.error4}>{error}</div>}
+
+                <button className={`${styles.loginBtn} ${styles.loginBtnMargin} ${props.loading?styles.btnLoading:''}`}>{props.loading?<LoadingIcon class={styles.loading}/>:"Zaloguj się"}</button>
 
             </form>
 
             <div className={styles.line}></div>
 
-            <button className={styles.googleLogin}>
+            <button className={`${styles.googleLogin} ${props.loading?styles.googleLoginWhileLoading:''}`} onClick={e=>!props.loading && googleLogin()}>
                 <GoogleIcon class={styles.googleSVG}/>
                 Zaloguj się przez Google
             </button>
 
             <div className={styles.bottomLink}>
-                Nie masz konta? <span className={styles.link} onClick={e=>displayLoginContext.setDisplayLogin('register')}>
+                Nie masz konta? <span className={`${styles.link} ${props.loading?styles.linkWhileLoading:''}`} onClick={e=>!props.loading && displayLoginContext.setDisplayLogin('register')}>
                     Zarejestruj się!
                 </span>
             </div>
